2609abcbc896d0acb3093291fde644064712349c
Anders Schütt <anders@nntq.net>
2022-11-29 15:22:55 +0100

    Update Task forms

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,5 +1,6 @@
 <x-template.form-page :action="route('iterations.tasks.store', compact('iteration'))" :header="'New Task for ' . $iteration->name">
     <x-forms.input name="name"/>
+    <x-forms.input name="description"/>
 
     <button type="submit" class="btn btn-primary">Create</button>
 </x-template.form-page>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,3 +1,7 @@
-<x-app>
+<x-template.form-page :action="route('tasks.update', compact('task'))" header="Edit task">
+    <x-forms.input name="name" :model="$task"/>
 
-</x-app>
+    <x-forms.input name="description" :model="$task"/>
+
+    <button type="submit" class="btn btn-primary">Create</button>
+</x-template.form-page>
