06b92b20cd08ce2ae50406e4714168a7a2dd04aa
Anders Schütt <anders@nntq.net>
2022-12-09 11:45:39 +0100

    Add missing migrations (oops)

---
+++ database/migrations/2022_12_09_033837_add_user_id_to_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->after('iteration_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
