9114a0175a2778c13fedc4e13dd6456a7325934d
Anders Schütt <anders@nntq.net>
2022-12-09 14:25:11 +0100

    Update seeded Users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,13 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $admin = User::factory()->create([
+            'is_admin' => true,
+            'name' => 'Administrator',
+            'email' => 'admin@atlas.dk',
+        ]);
+
         $user = User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'john@atlas.dk',
         ]);
 
         $project = Project::factory()->create();
-        $project->users()->sync([$user->id]);
+        $project->users()->sync([$user->id, $admin->id]);
 
         $iteration = Iteration::factory()->create();
         Task::factory(10)->create();
